a20a4ce32fef8e6f0e21f7c3224a60180ff250b2
Flashokiller <75789819+flashokiller@users.noreply.github.com>
2021-05-26 15:11:43 +0530

    Update ping.py

--- SaitamaRobot/modules/ping.py
+++ SaitamaRobot/modules/ping.py
@@ -74,13 +74,13 @@ def ping(update: Update, context: CallbackContext):
     msg = update.effective_message
 
     start_time = time.time()
-    message = msg.reply_text("Pinging...")
+    message = msg.reply_text("Pinging... Wait for decades till this end")
     end_time = time.time()
     telegram_ping = str(round((end_time - start_time) * 1000, 3)) + " ms"
     uptime = get_readable_time((time.time() - StartTime))
 
     message.edit_text(
-        "PONG!!\n"
+        "BONG!!\n"
         "<b>Time Taken:</b> <code>{}</code>\n"
         "<b>Service uptime:</b> <code>{}</code>".format(telegram_ping, uptime),
         parse_mode=ParseMode.HTML)
@@ -94,7 +94,7 @@ def pingall(update: Update, context: CallbackContext):
     pinged_list.insert(2, '')
     uptime = get_readable_time((time.time() - StartTime))
 
-    reply_msg = "⏱Ping results are:\n"
+    reply_msg = "┻━┻︵└(´_｀└)Fang results are:\n"
     reply_msg += "\n".join(pinged_list)
     reply_msg += '\n<b>Service uptime:</b> <code>{}</code>'.format(uptime)
 
