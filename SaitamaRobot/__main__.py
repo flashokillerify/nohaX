4fd101614bf520e3df323b4ffb2aedb186434c21
Flashokiller <75789819+flashokiller@users.noreply.github.com>
2021-05-29 08:30:26 +0530

    Update __main__.py

--- SaitamaRobot/__main__.py
+++ SaitamaRobot/__main__.py
@@ -586,7 +586,7 @@ def main():
             updater.bot.set_webhook(url=URL + TOKEN)
 
     else:
-        LOGGER.info("Using long polling.")
+        LOGGER.info("BABY I AM FELLING GOOD GIVE ME CHOCOLATE ;-;")
         updater.start_polling(timeout=15, read_latency=4, clean=True)
 
     if len(argv) not in (1, 3, 4):
@@ -598,6 +598,6 @@ def main():
 
 
 if __name__ == '__main__':
-    LOGGER.info("Successfully loaded modules: " + str(ALL_MODULES))
+    LOGGER.info("RUKO" + str(ALL_MODULES))
     telethn.start(bot_token=TOKEN)
     main()
