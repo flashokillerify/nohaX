6b45740485683c1e7650a71f42dc7ec6d1d2711f
Flashokiller <75789819+flashokiller@users.noreply.github.com>
2021-08-01 19:00:39 +0530

    Update telegraph.py

--- SaitamaRobot/modules/telegraph.py
+++ SaitamaRobot/modules/telegraph.py
@@ -8,7 +8,7 @@ from datetime import datetime
 from telegraph import Telegraph, upload_file, exceptions
 flashokiller = "noha"
 telegraph = Telegraph()
-r = telegraph.create_account(short_name=flasho)
+r = telegraph.create_account(short_name=flashokiller)
 auth_url = r["auth_url"]
 
 
